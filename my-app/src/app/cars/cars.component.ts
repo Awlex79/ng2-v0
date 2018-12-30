837141da77e488663935954b76d481b1f2fbfeaa
Awlex79 <36120830+Awlex79@users.noreply.github.com>
2018-12-29 23:12:09 -0600

    First commit

---
+++ my-app/src/app/cars/cars.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cars',
+  templateUrl: './cars.component.html',
+  styleUrls: ['./cars.component.css']
+})
+
+
+export class CarsComponent {
+  addCarStatus = '';
+  inputText = 'Default text';
+
+  constructor() {
+  }
+  addCar() {
+    this.addCarStatus = 'The car added!';
+  }
+
+  onKeyUp(event) {
+
+    this.inputText = event.target.value;
+  }
+
+}
